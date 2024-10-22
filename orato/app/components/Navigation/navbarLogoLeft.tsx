94246ce3eb350b8df50915a57ca6a23af5218e67
artofghost <67277022+Artnooijen@users.noreply.github.com>
2024-10-22 12:53:31 +0200

    updated the link styles and added some animations to make it smoother

--- orato/app/components/Navigation/navbarLogoLeft.tsx
+++ orato/app/components/Navigation/navbarLogoLeft.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+
 
 const NavbarLogoLeft = () => {
   return (
     <nav className="flex items-center ml-6">
       <Link href="/">
-        <Image src="/orato bloem.png" alt="Orato" width={50} height={50} priority/>
+        <div className="group flex items-center">
+          {/* Logo with spin animation on hover */}
+          <div className="relative">
+            <Image
+              src="/orato bloem.png"
+              alt="Orato"
+              width={50}
+              height={50}
+              priority
+              className="transition-transform duration-500 group-hover:rotate-12" // Add rotation on hover
+            />
+          </div>
+
+          {/* ORATO text with underline animation on hover */}
+          <div className="text-xl ml-2 font-bold relative inline-block">
+            <Link href="/" className="relative inline-block transition-all">
+              ORATO
+              {/* Underline effect like in NavbarMenuRight */}
+              <span className="absolute bottom-0 left-0 h-[2px] w-full origin-left scale-x-0 bg-black transition-transform duration-300 group-hover:scale-x-100" />
+            </Link>
+          </div>
+        </div>
       </Link>
-      <div className="text-xl ">
-        <Link href="/" className='text-xl ml-2 font-bold '>
-          ORATO
-        </Link>
-      </div>
     </nav>
   );
 };
--- orato/app/components/Navigation/navbarMenuRight.tsx
+++ orato/app/components/Navigation/navbarMenuRight.tsx
@@ -6,23 +6,28 @@ const NavbarMenuRight = () => {
   const pathname = usePathname();
 
   const NavbarMenuRightItems = [
-    { name: 'ARDIE', path: '/Onderwerpen/ardie' },
     { name: 'COACHING', path: '/Onderwerpen/Coaching' },
     { name: 'PRESENTEREN', path: '/Onderwerpen/Presenteren' },
     { name: 'SUPERVISIE', path: '/Onderwerpen/Supervisie' },
+    { name: 'ARDIE', path: '/Onderwerpen/ardie' },
   ];
 
   return (
     <div className="p-2 mr-14 mt-6">
-      <ul className="flex flex-col">
+      <ul className="flex flex-col space-y-0 z-50">
         {NavbarMenuRightItems.map((item) => (
-          <li key={item.name} className="">
-            <Link href={item.path} className={`block text-lg transition-all hover:border-b-2 hover:border-b-gray-700 hover:animate-borderLine 
-                ${pathname === item.path ? 'border-b-2 border-b-black' : ''}`}
+          <li key={item.name}>
+            <Link
+              href={item.path}
+              className={`relative inline-block text-lg font-medium text-gray-800 transition-all 
+                hover:text-black 
+                before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-full before:origin-left before:scale-x-0 
+                before:bg-black before:transition-transform before:duration-300 
+                hover:before:scale-x-100 
+                ${pathname === item.path ? 'before:scale-x-100 text-black' : ''}`}
             >
               {item.name}
             </Link>
-            
           </li>
         ))}
       </ul>
