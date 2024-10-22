0d858a2c204dfdb7865b2e9aba50f52a344dbe1a
artofghost <67277022+Artnooijen@users.noreply.github.com>
2024-10-22 11:20:34 +0200

    chore: Update navbar logo and text styling in navbarLogoLeft component

--- orato/app/page.tsx
+++ orato/app/page.tsx
@@ -19,58 +19,89 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
 
-      {/* Page layout */}
-      <div className="flex flex-col min-h-screen">
-        {/* Main content */}
-        <main className="flex-grow flex items-center justify-center p-4 ">
-          <div className="flex static flex-col md:flex-row justify-center items-center text-center md:text-left md:space-x-8">
+{/* Page layout */}
+<div className="flex flex-col min-h-screen">
+  {/* Main content */}
+  <main className="flex-grow flex items-center justify-center p-4">
+    <div className="flex flex-col md:flex-row justify-center items-center md:items-start text-center md:text-left">
+      {/* Container for "WELKOM BIJ" and Subtext */}
+      <div className="flex flex-col items-center md:items-start mt-4 md:mt-0">
+        <div className="relative flex flex-col">
+          {/* WELKOM BIJ */}
+          <div
+            className={`${noto_Serif_Display.className}
+              text-5xl sm:text-6xl md:text-7xl lg:text-8xl
 
-            <div className="flex flex-col text-left items-center md:items-start mt-4 md:mt-0">
-              {/* #TODO! */}
-              <div
-                className={`${noto_Serif_Display.className}
- 
-                sm:text-xl
-                sm:ml-10 
-                
-                md:text-8xl 
-                md:ml-10
-
-                lg:text-9xl 
-                lg:ml-10
-
-                xl: text
-                xl:ml-10 
-                `}
-              >
-                WELKOM BIJ
-              </div>
-              <div className=" lg:absolute  bottom-1/2 right-1/4 text-sm sm:text-xs md:ml-96 md:text-xs lg:text-sm font-light mt-2 ">
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;HET
-                JUISTE ADRES VOOR COACHING
-                <br />
-                SUPERVISIE, PRESENTEREN EN COMMUNICEREN.
-              </div>
-              <div
-                className={` text-5xl font-bold -ml-10  sm:text-6xl md:text-8xl lg:text-9xl
-                   `}
-              >
-                ORATO
-              </div>
-              
-              <div
-                className={`${noto_Serif_Display.className} text-4xl ml-10
-                 font-normal sm:text-6xl md:text-8xl lg:text-9xl`}
-              >
-                VOOR JOU
-              </div>
-
-            </div>
+            `}
+          >
+            WELKOM BIJ
           </div>
-        </main>
+          {/* Subtext attached to "WELKOM BIJ" on larger screens */}
+          <div
+            className="
+              hidden md:block
+              absolute
+              bottom-0
+              right-0
+              transform translate-y-16 translate-x-48 
+              text-sm sm:text-xs md:text-sm lg:text-base
+              font-light
+              mt-2 md:mt-4
+              ml-4 sm:ml-8 md:ml-12 lg:ml-32
+              max-w-xs sm:max-w-sm md:max-w-md
+              text-right
+            "
+          >
+            HET JUISTE ADRES VOOR COACHING
+            <br />
+            SUPERVISIE, PRESENTEREN EN COMMUNICEREN.
+          </div>
+        </div>
 
-        {/* Footer */}
+        {/* ORATO */}
+        <div
+          className="
+            text-7xl sm:text-8xl md:text-9xl lg:text-10xl
+            font-bold
+
+            -ml-4 sm:-ml-8 md:-ml-16 lg:-ml-24
+          "
+        >
+          ORATO
+        </div>
+
+        {/* VOOR JOU */}
+        <div
+          className={`${noto_Serif_Display.className}
+            text-6xl sm:text-7xl md:text-8xl lg:text-9xl            
+          `}
+        >
+          VOOR JOU
+        </div>
+
+        {/* Subtext below "VOOR JOU" on smaller screens */}
+        <div
+          className="
+            md:hidden
+            text-sm sm:text-base md:text-lg lg:text-xl
+            font-light
+            mt-4
+            ml-0 sm:ml-4
+            max-w-xs sm:max-w-sm
+            text-center
+          "
+        >
+          HET JUISTE ADRES VOOR COACHING
+          <br />
+          SUPERVISIE, PRESENTEREN EN COMMUNICEREN.
+        </div>
       </div>
+    </div>
+  </main>
+  {/* Footer */}
+</div>
+
+
       <footer className="bg-gray-800 text-white p-4 text-center">
         <p>&copy; 2024 Orato. All rights reserved.</p>
       </footer>
