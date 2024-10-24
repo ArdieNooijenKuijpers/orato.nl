5d5594101f64b5870bff82116c0a15ae430ba389
artofghost <67277022+Artnooijen@users.noreply.github.com>
2024-10-24 14:15:31 +0200

    Added aceternity and shad3cn
    feat: Add utility function for merging CSS class names
    
    The code changes introduce a new utility function `cn` in the `utils.ts` file. This function uses the `clsx` and `twMerge` libraries to merge multiple CSS class names into a single string. This improves the code's readability and maintainability by providing a convenient way to combine class names in a consistent manner.

--- orato/tailwind.config.ts
+++ orato/tailwind.config.ts
@@ -1,13 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
       colors: {
         'orato-orange': '#ee7901',
         'orato-green': '#77b829',
@@ -22,10 +42,13 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
         'spin-tick': 'spin 30s steps(30) infinite',
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
