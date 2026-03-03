5f23ef6b9d1904351756f95c07d00724690fd8f0
Art Nooijen <a.t.g.nooijen@gmail.com>
2026-03-03 09:42:49 +0100

    fixed the z index so the rotate hover works again

--- orato/app/components/Navigation/navbar.tsx
+++ orato/app/components/Navigation/navbar.tsx
@@ -5,7 +5,7 @@ import NavbarMenuRight from "./navbarMenuRight"
 const Navbar = () => {
   return (
     <div>
-        <div className="fixed top-0 left-0 w-full z-10 flex justify-between pointer-events-none">
+        <div className="fixed top-0 left-0 w-full z-30 flex justify-between pointer-events-none">
             <div className="pointer-events-auto">
                 <NavbarLogoLeft showText={false} />
             </div>
